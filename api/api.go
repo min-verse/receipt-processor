2b12279b49e0a5991771eceef8159696093c2da7
Min Tu <biffboffkev@gmail.com>
2024-11-25 22:49:27 -0500

    Initial commit that contains the Error struct and error handling methods inside api.go

---
+++ api/api.go
@@ -0,0 +1,44 @@
+package api
+
+import(
+	"encoding/json"
+	"net/http"
+)
+
+// Error Response
+type Error struct{
+	// Error Code
+	Code int
+
+	// Error Message
+	Message string
+}
+
+// Function to write an error that will be inherited in multiple places
+func writeError(w http.ResponseWriter, message string, code int){
+	// Creating an Error Struct
+	var resp Error = Error{
+		Code: code,
+		Message: message,
+	}
+
+	// Sets the response header "Content-Type" as "application/json"
+	w.Header().Set("Content-Type", "application/json")
+	// Sets the error code in the header
+	w.WriteHeader(code)
+
+	// Put the Error Struct from above as a JSON response
+	// Initializes the w http.ResponseWriter as a NewEncoder and then encodes the Error Struct
+	json.NewEncoder(w).Encode(resp)
+}
+
+var(
+	// When the user does something that results in an error
+	RequestErrorHandler = func(w http.ResponseWriter, err error){
+		writeError(w, err.Error(), http.StatusBadRequest)
+	}
+	// When the server internally runs into an error
+	InternalErrorHandler = func(w http.ResponseWriter){
+		writeError(w, "An Unexpected Internal Error Occurred", http.StatusInternalServerError)
+	}
+)
